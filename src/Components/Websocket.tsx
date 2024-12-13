6781a8fe6e27a9cd8bdf02b680a17373b115c0e8
nika <nika3141535@gmail.com>
2024-12-13 22:21:11 +0400

    fix:add checking expired token 6

--- src/Components/Websocket.tsx
+++ src/Components/Websocket.tsx
@@ -5,6 +5,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import useWebSocket from "react-use-websocket";
+import { jwtDecode } from "jwt-decode";
 
 
 
@@ -19,8 +20,23 @@ export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({ chi
   const token = Cookies.get("token");
 
   // Fetch the WebSocket token
+
+  const isTokenExpired = (token:string) => {
+      try {
+          const decoded = jwtDecode(token);
+          const currentTime = Math.floor(Date.now() / 1000);
+          if(decoded.exp){ // Current time in seconds
+          return decoded.exp > currentTime
+          }
+          return false // Check if the token is expired
+      } catch (error) {
+          console.error("Invalid token:", error);
+          return true; // Treat as expired if decoding fails
+      }
+  };
+
   useEffect(() => {
-    if(token && token != 'logout')triggerConnection();
+    if(token && token != 'logout' && isTokenExpired(token))triggerConnection();
   }, [token]);
 
   const { sendJsonMessage, lastJsonMessage } = useWebSocket(
