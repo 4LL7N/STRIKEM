dcfd5f97bb32c43dab1be5c34a9b89d9fbe6fdd5
nika <nika3141535@gmail.com>
2025-01-20 22:30:19 +0400

    fix:errors of email verification

--- src/Components/UsersMemo/EmailVerifivation.tsx
+++ src/Components/UsersMemo/EmailVerifivation.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function EmailVerifivation() {
-  const [verified, setVerified] = useState<boolean>(false);
+  const [verified, setVerified] = useState<number>(0);
   const { uid, token } = useParams<{ uid: string; token: string }>();
 
   const verifyEmail = async () => {
@@ -12,16 +12,19 @@ function EmailVerifivation() {
         `https://strikem.site/users/activate/${uid}/${token}/`
       );
       if (response.status === 200) {
-        setVerified(true);
+        setVerified(1);
+      }else{
+        setVerified(2)
       }
     } catch (err) {
       console.log(err);
+      setVerified(2)
     }
   };
 
   useEffect(()=>{
     if(!uid || !token){
-        setVerified(false)
+        setVerified(2)
          return
     }
     verifyEmail()
@@ -29,16 +32,20 @@ function EmailVerifivation() {
 
   return (
     <div className="bg-[#10141E]  pt-[10%]  w-[100vw] h-[100vh] ">
-        {verified ? 
+        {verified == 1 ? 
             <div className="flex flex-col gap-6 items-center" >
             <h1 className="text-white text-6xl ">Email is verified</h1>
             <p className="text-white text-4xl ">go back to main site</p>
           </div>
           :
+          verified == 0?
           <div className="flex flex-col gap-6 items-center" >
             <h1 className="text-white text-6xl ">Verifying...</h1>
           </div>
-
+          :
+          <div className="flex flex-col gap-6 items-center" >
+          <h1 className="text-white text-6xl ">Something went wrong</h1>
+        </div>
         }
       
     </div>
