55656133eaeec358a0737bd3fbba00a2a2fccf26
nika <nika3141535@gmail.com>
2025-01-25 18:31:21 +0400

    add:uploadRatingBox in to redux store

--- src/ReduxStore/store.ts
+++ src/ReduxStore/store.ts
@@ -3,12 +3,14 @@ import {configureStore} from '@reduxjs/toolkit';
 import reservationBox from './features/reservationBox';
 import unreadMatchUps from './features/unReadMatchups';
 import userLogIn from './features/userLogIn'
+import uploadRatingBox  from './features/uploadRatingBox';
 
 export const store = configureStore({
     reducer: {
         reservationBox,
         unreadMatchUps,
-        userLogIn
+        userLogIn,
+        uploadRatingBox
     }
 })
 
