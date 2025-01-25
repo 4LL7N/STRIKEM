47fc2018d8180511a72ecfe39c207c30946f5103
nika <nika3141535@gmail.com>
2025-01-26 03:53:01 +0400

    add:allReviews to redux store

--- src/ReduxStore/store.ts
+++ src/ReduxStore/store.ts
@@ -5,6 +5,7 @@ import unreadMatchUps from './features/unReadMatchups';
 import userLogIn from './features/userLogIn'
 import uploadRatingBox  from './features/uploadRatingBox';
 import currentUser from './features/currentUser';
+import allReviewsBox from './features/allReviewsBox';
 
 export const store = configureStore({
     reducer: {
@@ -12,7 +13,8 @@ export const store = configureStore({
         unreadMatchUps,
         userLogIn,
         uploadRatingBox,
-        currentUser
+        currentUser,
+        allReviewsBox 
     }
 })
 
